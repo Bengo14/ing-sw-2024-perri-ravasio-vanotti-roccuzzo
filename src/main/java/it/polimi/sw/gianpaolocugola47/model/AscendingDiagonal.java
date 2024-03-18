0108e15c337b3c24489b23a364440dc80711716c
Paolo <paolo1.vanotti@mail.polimi.it>
2024-03-18 18:33:19 +0100

    Added comments

--- src/main/java/it/polimi/sw/gianpaolocugola47/model/AscendingDiagonal.java
+++ src/main/java/it/polimi/sw/gianpaolocugola47/model/AscendingDiagonal.java
@@ -1,5 +1,8 @@
 package it.polimi.sw.gianpaolocugola47.model;
-
+/**
+ * This class is a subclass of Lshape which is a subclass of Pattern
+ * It looks for a specific pattern of cards to fulfill an objective
+ */
 public class AscendingDiagonal extends Pattern {
 
     public AscendingDiagonal(Resources mainResource){

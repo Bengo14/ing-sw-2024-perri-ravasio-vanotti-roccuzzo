464049d94afbb254c8eff7a4a7b3a71a77435893
davide <10743170@polimi.it>
2024-03-24 16:47:11 +0100

    test class correction

--- src/test/java/it/polimi/sw/gianpaolocugola47/model/CornerTest.java
+++ src/test/java/it/polimi/sw/gianpaolocugola47/model/CornerTest.java
@@ -9,19 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class CornerTest {
     @Test
     public void testConstructorAndGetterCorner() {
-        Corner c = new Corner(false,false);
+        Corner c = new Corner(false, false, false);
         assertNotNull(c);
         assertFalse(c.isBuildable());
-        assertFalse(c.isEmpty());
+        assertFalse(c.isItem());
+        assertFalse(c.isResource());
         assertFalse(c.isCovered());
-        assertTrue(c.isCovered());
         assertNull(c.getLinkedCorner());
-        c.setLinkedCorner(new Corner(true,true));
+        c.setLinkedCorner(new Corner(true,true,true));
         assertNotNull(c.getLinkedCorner());
     }
     @Test
     public void testSetIsCovered() {
-        Corner c = new Corner(false,false);
+        Corner c = new Corner(false,false,false);
         assertFalse(c.isCovered());
         c.setIsCovered();
         assertTrue(c.isCovered());
--- src/test/java/it/polimi/sw/gianpaolocugola47/model/ObjectivesTest.java
+++ src/test/java/it/polimi/sw/gianpaolocugola47/model/ObjectivesTest.java
@@ -6,54 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectivesTest {
     @Test
-    public void testConstructor() {
-        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack");
+    public void testConstructorAndGetterObjectives() {
+        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack") {
+            @Override
+            public int checkPatternAndComputePoints(PlayerTable playerTable) {
+                return 0;
+            }
+        };
         assertNotNull(o);
         assertEquals(5, o.getPoints());
         assertEquals("imgPathFront", o.getImgPathFront());
         assertEquals("imgPathBack", o.getImgPathBack());
-    }
-
-
-    @Test
-    void setPoints() {
-    }
-
-
-    @Test
-    public void getItems() {
-        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack");
-        assertNotNull(o.getItems());
-    }
-
-    @Test
-    public void setItems() {
-        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack");
-        o.setItems(null);
-        assertNull(o.getItems());
-    }
-
-    @Test
-    public void getResources() {
-        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack");
-        assertNotNull(o.getResources());
-    }
-
-    @Test
-    public void setResources() {
-        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack");
-        o.setResources(null);
-        assertNull(o.getResources());
-    }
-
-    @Test
-    public void getPattern() {
-        Objectives o = new Objectives(5, "imgPathFront", "imgPathBack");
-        assertNotNull(o.getPattern());
-    }
-
-    @Test
-    public void checkPatternAndComputePoints() {
-
     }
 }

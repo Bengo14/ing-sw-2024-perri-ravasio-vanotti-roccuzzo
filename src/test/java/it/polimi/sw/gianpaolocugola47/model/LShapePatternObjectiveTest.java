89f864bc2653cc172e924fd29e7c7e18ee3baf16
davide <10743170@polimi.it>
2024-04-28 14:36:17 +0200

    update Lshape test

--- src/test/java/it/polimi/sw/gianpaolocugola47/model/LShapePatternObjectiveTest.java
+++ src/test/java/it/polimi/sw/gianpaolocugola47/model/LShapePatternObjectiveTest.java
@@ -14,33 +14,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LShapePatternObjectiveTest {
 
-    //in commento tanto non serve ad aumentare la coverage
-//    @Test
-//    public void testConstructor() {
-//        LShapePatternObjective obj = objectiveCardsDeck.get(12);
-//        assertNotNull(obj);
-//        assertEquals(3, obj.getPoints());
-//        assertEquals("imgPathFrn", obj.getImgPathFront());
-//        assertEquals("imgPathBck", obj.getImgPathBack());
-//
-//    }
-//    @Test
-//    public void testCheckPatternAndComputePoints() {
-//        LShapePatternObjective obj = objectiveCardsDeck.get(12);
-//        //create 4 card from json file all in back front
-//        StartingCard start = startingCardsDeck.get(0);
-//        ResourceCard resource_1= resourceCardsDeck.get(0);
-//        ResourceCard resource_2= resourceCardsDeck.get(1);
-//        ResourceCard resource_3= resourceCardsDeck.get(2);
-//        ResourceCard resource_4= resourceCardsDeck.get(3);
-//        //sett the card on the table
-//
-//        PlayerTable playerTable = new PlayerTable(1,"name",new ResourceCard[]{});
-//        start.isFront();
-//        start.setCoordinates(29,29);
-//        //set card on back front
-//
-//
-//        assertEquals(0, obj.checkPatternAndComputePoints(playerTable));
-//    }
-}
+
+    @Test
+    public void testConstructor() {
+        LShapePatternObjective obj = new LShapePatternObjective(3, "imgPathFront", "imgPathBack", "topLeft", Resources.PLANT, Resources.INSECTS);
+        assertEquals(3, obj.getPoints());
+        assertEquals("imgPathFront", obj.getImgPathFront());
+        assertEquals("imgPathBack", obj.getImgPathBack());
+
+    }
+    @Test
+    public void testCheckPatternAndComputePoints(){
+        MainTable main = new MainTable();
+
+        Deck.initDeck();
+        main.setNumOfPlayers(2);
+        StartingCard start = Deck.getStartingCardsDeck().get(2);
+        ResourceCard plant_1 = Deck.getResourceCardsDeck().get(6);
+        ResourceCard plant_2 = Deck.getResourceCardsDeck().get(7);
+        ResourceCard insect = Deck.getResourceCardsDeck().get(2);
+        LShapePatternObjective obj = (LShapePatternObjective) Deck.getObjectiveCardsDeck().get(15);
+        PlayerTable player = new PlayerTable(1, "name", new ResourceCard[]{plant_1, plant_2, insect});
+        main.setPlayerTable(1,player);
+        main.setPlayerSecretObjective(1,obj);
+        start.switchFrontBack();
+        main.setPlayerStartingCard(1,start);
+
+
+
+
+
+
+
+        main.playCardAndUpdatePoints(0,29,29,0,1);
+
+        main.playCardAndUpdatePoints(1,29,29,2,1);
+
+
+        main.playCardAndUpdatePoints(2,30,28,2,1);
+
+        System.out.println(obj.checkPatternAndComputePoints(player));
+
+
+    }
+    @Test
+    public void testIsSecondaryResourceMatchedAndNotFlagged(){
+
+
+    }
+
+
+
+ }
+

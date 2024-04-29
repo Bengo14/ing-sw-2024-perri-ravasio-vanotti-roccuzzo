79a8bef4e5bd6363b7acff3c799553a12ae81ac4
davide <10743170@polimi.it>
2024-04-29 14:51:12 +0200

    class test

--- src/test/java/it/polimi/sw/gianpaolocugola47/model/DiagonalPatternObjectiveTest.java
+++ src/test/java/it/polimi/sw/gianpaolocugola47/model/DiagonalPatternObjectiveTest.java
@@ -1,45 +1,8 @@
 package it.polimi.sw.gianpaolocugola47.model;
-
-
 import org.junit.jupiter.api.Test;
-
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class DiagonalPatternObjectiveTest {
-
-
-    @Test
-    public void testConstructorFungi() {
-        Deck.initDeck();
-        //create the card from json file
-        DiagonalPatternObjective f = (DiagonalPatternObjective) Deck.getObjectiveCardsDeck().get(8);
-        assertNotNull(f);
-        assertEquals(true, f.isAscending());
-        assertEquals(Resources.FUNGI, f.getResource());
-    }
-    @Test
-    public void testConstructorAnimal() {
-        Deck.initDeck();
-
-        //create the card from json file
-        DiagonalPatternObjective a = (DiagonalPatternObjective) Deck.getObjectiveCardsDeck().get(9);
-        assertNotNull(a);
-        assertEquals(true, a.isAscending());
-        assertEquals(Resources.ANIMAL, a.getResource());
-    }
-    @Test
-    public void testConstructorPlant() {
-        Deck.initDeck();
-
-        //create the card from json file
-        DiagonalPatternObjective p = (DiagonalPatternObjective) Deck.getObjectiveCardsDeck().get(10);
-        assertNotNull(p);
-        assertEquals(false, p.isAscending());
-        assertEquals(Resources.PLANT, p.getResource());
-    }
     @Test
     public void testConstructor() {
         DiagonalPatternObjective obj = new DiagonalPatternObjective(2, "imgPathFront", "imgPathBack", false, Resources.INSECTS);
@@ -50,31 +13,22 @@ class DiagonalPatternObjectiveTest {
     @Test
     public void isResourceMatchedAndNotFlagged() {
         Deck.initDeck();
-
         DiagonalPatternObjective obj = (DiagonalPatternObjective) Deck.getObjectiveCardsDeck().get(11);
         PlayerTable player = new PlayerTable(1, "name", new ResourceCard[]{});
-
-
-
     }
     @Test
     void testIsResourceMatchedAndNotFlagged() {
         Deck.initDeck();
-
-        // Test when the resource matches and the card is not flagged
         DiagonalPatternObjective obj = (DiagonalPatternObjective) Deck.getObjectiveCardsDeck().get(11);
         ResourceCard resourceCard = Deck.getResourceCardsDeck().get(2);
         assertTrue(obj.isResourceMatchedAndNotFlagged(resourceCard));
-
         // Test when the resource does not match
         ResourceCard differentResourceCard = new ResourceCard("back", "front", 1, Resources.ANIMAL);
         assertFalse(obj.isResourceMatchedAndNotFlagged(differentResourceCard));
-
         // Test when the card is flagged
         resourceCard.setFlaggedForObjective(true);
         assertFalse(obj.isResourceMatchedAndNotFlagged(resourceCard));
     }
-
     @Test
     public void testCheckPatternAndComputePoints(){
         MainTable main = new MainTable();
@@ -95,5 +49,4 @@ class DiagonalPatternObjectiveTest {
         System.out.println(obj.checkPatternAndComputePoints(player));
         assertEquals(2, obj.checkPatternAndComputePoints(player));
     }
-
 }
--- src/test/java/it/polimi/sw/gianpaolocugola47/model/GoldCardTest.java
+++ src/test/java/it/polimi/sw/gianpaolocugola47/model/GoldCardTest.java
@@ -26,9 +26,7 @@ class GoldCardTest {
     }
     @Test
     public void testGetresourcedRequired() {
-
         Deck.initDeck();
-
         GoldCard gold = Deck.getGoldCardsDeck().get(0);
         gold.getResourcesRequired();
         System.out.println(gold.getResourcesRequired());
@@ -39,28 +37,18 @@ class GoldCardTest {
         MainTable main = new MainTable();
         Deck.initDeck();
         GoldCard g = Deck.getGoldCardsDeck().get(0);
-
-
         ResourceCard plant_1 = Deck.getResourceCardsDeck().get(12);
-
         ResourceCard plant_2 = Deck.getResourceCardsDeck().get(7);
-
         StartingCard start = Deck.getStartingCardsDeck().get(2);
-
-
         Objectives obj = Deck.getObjectiveCardsDeck().get(0);
         main.setNumOfPlayers(2);
-
         PlayerTable p = new PlayerTable(1, "name",new ResourceCard[]{plant_1,plant_2,g});
-
         main.setPlayerTable(1,p);
         main.switchCardOnHandFrontBack(1,1);
         main.switchCardOnHandFrontBack(1,0);
         main.switchCardOnHandFrontBack(1,2);
         main.setPlayerStartingCard(1,start);
         main.setPlayerSecretObjective(1,obj);
-
-
         main.playCardAndUpdatePoints(0,29,29,0,1);
         System.out.println(p.getResourceCounter(0));
         main.playCardAndUpdatePoints(1,29,29,1,1);
@@ -72,12 +60,8 @@ class GoldCardTest {
         System.out.println(p.getResourceCounter(1));
         System.out.println(p.getResourceCounter(2));
         System.out.println(p.getResourceCounter(3));
-
         System.out.println(main.getBoardPoints(1));
         assertEquals(5, main.getBoardPoints(1));
-
-
-
     }
     @Test
     public void testGetCardPointForCorners() {
@@ -99,8 +83,6 @@ class GoldCardTest {
         main.playCardAndUpdatePoints(1, 29, 29, 1, 1);
         main.playCardAndUpdatePoints(2, 28, 28, 1, 1);
         assertEquals(4, main.getBoardPoints(1));
-
-
     }
     @Test
     public void testGetCardPointForItems(){
@@ -140,7 +122,6 @@ class GoldCardTest {
        main.setPlayerStartingCard(1, start);
        main.setPlayerSecretObjective(1,obj);
        main.playCardAndUpdatePoints(0, 29, 29, 1, 1);
-
     }
 
 
